40969162537e7ab518bdab04a5f60cbae0e6387f
firmcode <pig6775@naver.com>
2016-08-05 05:24:46 -0700

    back

--- sniff.py
+++ sniff.py
@@ -4,14 +4,20 @@ from scapy.all import *
 import sys
 from uuid import getnode as get_mac
 
-Myip= "192.168.218.174"
-Mymac = "00:0c:29:d7:80:2d"
-Senderip = "192.168.218.151"
-Sendermac = "00:0C:29:10:23:d3"
-Receiverip = "192.168.218.2"
-Receivermac = "00:50:56:ff:4b:b4"
-testmac = "00:50:56:aa:aa:aa"
-testmac1 = "00:50:56:bb:bb:bb"
+#Myip= "192.168.218.174"
+#Mymac = "00:0c:29:d7:80:2d"
+#Senderip = "192.168.218.151"
+#Sendermac = "00:0C:29:10:23:d3"
+#Receiverip = "192.168.218.2"
+#Receivermac = "00:50:56:ff:4b:b4"
+
+Myip =""
+Mymac=""
+Senderip=""
+Seddermac=""
+Receiverip=""
+Receivermac=""
+
 
 raw_packet_cache = None
 
@@ -21,9 +27,9 @@ def pkt_callback(pkt):
 	#	return
 	if pkt[Ether].dst!=Mymac:
 		return
+
 	if ARP in pkt:
 		return
-
 	if pkt[0].haslayer(UDP):
 		if pkt[IP].src==Senderip:
                         pkt[Ether].src=Mymac
@@ -32,6 +38,7 @@ def pkt_callback(pkt):
 			del pkt[UDP].chksum
                         del pkt[UDP].len
                         del pkt.chksum
+                        print "send"
                         pkt = pkt.__class__(str(pkt))
                         sendp(pkt)
 
@@ -43,6 +50,7 @@ def pkt_callback(pkt):
 			del pkt[UDP].chksum
                         del pkt.chksum
                         pkt = pkt.__class__(str(pkt))
+                        print "send"
                         sendp(pkt)
 
 	else :
@@ -56,6 +64,8 @@ def pkt_callback(pkt):
 			#print pkt.show2()
 			pkt = pkt.__class__(str(pkt))
 			#ipkt.display()
+                        print "send"
+
 			sendp(pkt)
 			#print "send!!!"	
 
@@ -67,9 +77,37 @@ def pkt_callback(pkt):
                         #del pkt[TCP].chksum
                         del pkt.chksum
                         pkt = pkt.__class__(str(pkt))
+                        print "send"
+
                         sendp(pkt)
+
+
+#Get MY Address!!!!!!!!!!!!
+strs = subprocess.check_output(shlex.split('ip r l'))
+Myip  = strs.split('src')[-1].split()[0]
+Mymac = get_mac()
+Mymac =':'.join(("%012X" % Mymac)[i:i+2] for i in range(0, 12, 2))
+Mymac= Mymac.lower()
+print "Myip :"+ Myip+"  Mymac :"+Mymac
+
+
+#Get Receiver Address!!!!!!!!!!!
+Receiverip = strs.split('default via')[-1].split()[0]
+send(ARP(op=1, pdst=Receiverip, psrc=Myip, hwdst="ff:ff:ff:ff:ff:ff"))
+result, unanswered = sr(ARP(op=ARP.who_has, pdst=Receiverip))
+Receivermac = result[0][1].hwsrc
+print "Receiverip : "+Receiverip+" Receivermac : "+Receivermac
+
+#Get Sender Address!!!!!!!!!!!
+Senderip=raw_input("Enter SenderIP : ")
+print Senderip
+send(ARP(op=1, pdst=Senderip, psrc=Myip, hwdst="ff:ff:ff:ff:ff:ff"))
+result, unanswered = sr(ARP(op=ARP.who_has, pdst=Senderip))
+Sendermac=result[0][1].hwsrc
+print "Senderip : "+Senderip+" Sendermac : "+Sendermac
+
 
 
 DefFilter = "host "+Receiverip+" or host "+Senderip
 print "hello"
-sniff(prn=pkt_callback, filter=DefFilter, store=1)
+sniff(prn=pkt_callback, lfilter=lambda d: d.dst == Mymac, store=1)
